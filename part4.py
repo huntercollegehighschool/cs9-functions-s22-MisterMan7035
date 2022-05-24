536ee503df76208f3fc74b6957e0355a947bc49b
David Li <103528452+MisterMan7035@users.noreply.github.com>
2022-05-24 00:41:21 +0000

    Completed part 4

--- part4.py
+++ part4.py
@@ -11,5 +11,17 @@ However, 2, 3, 5 doesn't work because 2 + 3 is not greater than 5.
 Define a function possibletriangle that takes 3 arguments (side1, side2, side3). The function RETURNS (not print) True (Boolean value, not the string, so no quotes necessary) if the sides satisfy the triangle inequality, and False (Boolean value, not the string, so no quotes necessary) if it does not. 
 '''
 
-def possibletriangle(side1, side2, side3): #do not change this line
-  pass #delete the word pass when you start writing your code
+def possibletriangle(side1, side2, side3): 
+  x = bool(0)
+  y = 0
+  z = 0
+  a = 0
+  if side1 + side2 > side3: 
+    y = 1
+  if side2 + side3 > side1:
+    z = 1
+  if side3 + side1 > side2:
+    a = 1
+  if y + z + a == 3:
+    x = bool(1)   
+  return x
